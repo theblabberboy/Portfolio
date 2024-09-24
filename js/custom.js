51834c7cd2c2143d8ca480cb2ba69194f19e6505
theblabberboy <rupam.astro@gmail.com>
2024-09-25 02:21:03 +0530

    Main Files added

---
+++ js/custom.js
@@ -0,0 +1,232 @@
+// JavaScript Document
+
+$(window).load(function () {
+    "use strict";
+    // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+});
+
+$(document).ready(function () {
+    "use strict";
+
+    // scroll menu
+    var sections = $('.section'),
+        nav = $('.navbar-fixed-top,footer'),
+        nav_height = nav.outerHeight();
+
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+
+        sections.each(function () {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+    });
+
+    nav.find('a').on('click', function () {
+        var $el = $(this),
+            id = $el.attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - nav_height + 2
+        }, 600);
+
+        return false;
+    });
+
+    // Menu opacity
+    if ($(window).scrollTop() > 80) {
+        $(".navbar-fixed-top").addClass("bg-nav");
+    } else {
+        $(".navbar-fixed-top").removeClass("bg-nav");
+    }
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 80) {
+            $(".navbar-fixed-top").addClass("bg-nav");
+        } else {
+            $(".navbar-fixed-top").removeClass("bg-nav");
+        }
+    });
+
+    // Parallax
+    var parallax = function () {
+        $(window).stellar();
+    };
+
+    $(function () {
+        parallax();
+    });
+
+    // AOS
+    AOS.init({
+        duration: 1200,
+        once: true,
+        disable: 'mobile'
+    });
+
+    // isotope
+    $('#projects').waitForImages(function () {
+        var $container = $('.portfolio_container');
+        $container.isotope({
+            filter: '*',
+        });
+
+        $('.portfolio_filter a').click(function () {
+            $('.portfolio_filter .active').removeClass('active');
+            $(this).addClass('active');
+
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 500,
+                    animationEngine: "jquery"
+                }
+            });
+            return false;
+        });
+    });
+
+    // animatedModal
+    $("#demo01").animatedModal({
+        modalTarget: 'recon'
+    });
+
+    $("#demo02").animatedModal({
+        modalTarget: 'emotion'
+    });
+
+    $("#demo03").animatedModal({
+        modalTarget: 'pulsar'
+    });
+
+    $("#demo04").animatedModal({
+        modalTarget: 'threebody'
+    });
+
+    $("#demo05").animatedModal({
+        modalTarget: 'galaxies'
+    });
+
+    $("#demo06").animatedModal({
+        modalTarget: 'star'
+    });
+
+    $("#demo07").animatedModal({
+        modalTarget: 'sun'
+    });
+
+    $("#demo08").animatedModal({
+        modalTarget: 'moire'
+    });
+
+    $("#demo09").animatedModal({
+        modalTarget: 'telepathy'
+    });
+
+    // Contact Form 	
+    // validate contact form
+    $(function () {
+        $('#contact-form').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email: {
+                    required: true
+                },
+                phone: {
+                    required: false
+                },
+                message: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "This field is required",
+                    minlength: "your name must consist of at least 2 characters"
+                },
+                email: {
+                    required: "This field is required"
+                },
+                message: {
+                    required: "This field is required"
+                }
+            },
+            submitHandler: function (form) {
+                $(form).ajaxSubmit({
+                    type: "POST",
+                    data: $(form).serialize(),
+                    url: "process.php",
+                    success: function () {
+                        $('#contact :input').attr('disabled', 'disabled');
+                        $('#contact').fadeTo("slow", 1, function () {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor', 'default');
+                            $('#success').fadeIn();
+                        });
+                    },
+                    error: function () {
+                        $('#contact').fadeTo("slow", 1, function () {
+                            $('#error').fadeIn();
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+    // Collapsible functionality
+    var coll = document.getElementsByClassName("collapsible");
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
+    // Award and Achievement
+    // Show the modal when an award link is clicked
+    $('.award-link').click(function(event) {
+        event.preventDefault();
+
+        // Get the modal target from the clicked link's href attribute
+        const targetModal = $(this).attr('href');
+
+        // Show the corresponding modal
+        $(targetModal).css('display', 'block');
+    });
+
+    // Close the modal when the close button is clicked
+    $('.close-awardModal').click(function() {
+        // Find the closest modal and hide it
+        $(this).closest('.modal').css('display', 'none');
+    });
+
+    // Close the modal if clicking outside of the modal content
+    $(window).click(function(event) {
+        if ($(event.target).hasClass('modal')) {
+            $(event.target).css('display', 'none');
+        }
+    });
+    
+});
